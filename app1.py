f7bcbc0c342a1334144f36129e2a8af6f2063a05
Pranav Chothe <chothe.pranav@dypic.in>
2026-01-05 12:54:26 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,23 +2,18 @@ import streamlit as st
 import os
 import google.generativeai as genai
 
-load_dotenv()
-
-client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
-
 st.title("🤖 Simple Gemini Chatbot")
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 user_input = st.text_input("Ask me anything:")
 
 if st.button("Send"):
     if user_input:
-        response = client.models.generate_content(
-            model="models/gemini-2.5-flash",
-            contents=user_input
-        )
+        response = model.generate_content(user_input)
         st.write("**Gemini Reply:**")
         st.write(response.text)
     else:
         st.warning("Please type a question")
-
-
